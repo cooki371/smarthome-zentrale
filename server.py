507e42f6ec9996aaf616a705c2ee6901ea633f47
Cooki371 <americancooki1@gmail.com>
2022-03-26 15:25:48 +0100

    uploaded first try for server.py

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import subprocess
+
+app = Flask(__name__)
+
+@app.route("/payload", methods=['POST'])
+def gitHubUpdate():
+    p = subprocess.Popen(['git','pull'], cwd='/home/smarthome')
+    p.wait()
+
+    return "Done!"
+
+if __name__ == "__main__":
+    app.run(port=80)
