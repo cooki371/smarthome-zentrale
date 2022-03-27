ac043a3e4761f5eb24f157078f20269510c1cef5
Cooki371 <americancooki1@gmail.com>
2022-03-27 18:06:04 +0200

    first try of SQL Query with python

---
+++ verification/verification_test_file.py
@@ -0,0 +1,23 @@
+import mysql.connector
+import databaseconfig as cfg
+
+db = mysql.connector.connect(
+    host=cfg.database["host"],
+    user=cfg.database["user"],
+    password=cfg.database["password"],
+    db=cfg.database["db"]
+)
+
+cursor = db.cursor()
+
+query = ("SELECT mail FROM user WHERE iduser={}")
+
+userid = 0
+
+cursor.execute(query.format(userid))
+
+for (iduser, mail, verified) in cursor:
+    print("{} {} {}".format(iduser, mail, verified))
+
+cursor.close()
+db.close()
