ca888b13d5ccff48950c4af5698f799a7e8f014a
Cooki371 <americancooki1@gmail.com>
2022-03-27 19:03:42 +0200

    fix string in Query

--- verification/start_verification.py
+++ verification/start_verification.py
@@ -4,12 +4,12 @@ cursor = dbcfg.db.cursor()
 
 
 def startVerify(mail):
-    cursor.execute("SELECT iduser FROM user WHERE mail = {}".format(mail))
+    cursor.execute("SELECT iduser FROM user WHERE mail = '{}'".format(mail))
 
     if cursor is None:
-        cursor.execute("INSERT INTO user (mail) VALUES ({})".format(mail))
+        cursor.execute("INSERT INTO user (mail) VALUES ('{}')".format(mail))
 
-        cursor.execute("SELECT iduser FROM user WHERE mail = {}".format(mail))
+        cursor.execute("SELECT iduser FROM user WHERE mail = '{}'".format(mail))
         for userid in cursor:
             iduser = userid
 
