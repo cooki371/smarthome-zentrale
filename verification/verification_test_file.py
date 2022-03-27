baa64e400b229bf0a1e6767b6c635c6985e8e3b7
Cooki371 <americancooki1@gmail.com>
2022-03-27 18:22:34 +0200

    testing test file?

--- verification/verification_test_file.py
+++ verification/verification_test_file.py
@@ -17,7 +17,7 @@ userid = 1
 cursor.execute(query.format(userid))
 
 for (mail,userid) in cursor:
-    print("Die Mail von user "+ userid + " ist " + mail)
+    print("Die Mail von User {} ist {}".format(userid, mail))
 
 cursor.close()
 db.close()
